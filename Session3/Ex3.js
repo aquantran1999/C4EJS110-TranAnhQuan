f61fac53fa6b310a55c15eb32b55310fd1bfbf17
Anh Quan <aquantran1999@gmail.com>
2021-03-23 17:59:57 +0700

    Ex3

---
+++ Session3/Ex3.js
@@ -0,0 +1,154 @@
+// // Bài 1: Give at least 2 examples to demonstrate you have acquired variable swap
+// // 1.1
+// // let a = 8;
+// // let b = 9;
+// // [a, b] = [b, a]; 
+// // console.log(a, b);
+// // // 1.2
+// // let a = 50;
+// // let b = 100;
+// // [a, b] = [b, a]; 
+// // console.log(a, b);
+// // // Bài 2: Example to demonstrate Split String 
+// // let a = "Hi my friend";
+// // let b = a.split(" ");
+// // console.log(b);
+// // // Bài 3: Log (print out) an array without using loops
+// // const a = [4, 5, 7, -8];
+// // console.log(...a);
+// // // Bài 4: Write a script to simulate a clothes shop, asking and performing certain tasks from users
+
+// // Bài 5
+{
+    let a = prompt('Nhập vào dãy số ngắn cách nhau bằng dấu phẩy(,) để tính tổng dãy số đó');
+    let b = a.split(",");
+    let sum = 0;
+    
+    for(i=0;i<b.length;i++)
+    {
+        b[i] = Number(b[i]);
+        sum +=b[i];   
+    }
+    console.log(`Tổng của dãy số đó là ${sum}`);
+}
+
+// // Bài 6
+// {
+//     let inNum = prompt('Nhập vào dãy số ngắn cách nhau bằng dấu phẩy(,) tìm số nhỏ nhất của dãy số đó');
+//     let arNum = inNum.split(",");
+    
+//     var maxInNumbers = Math.max.apply(Math, arNum); 
+//     var minInNumbers = Math.min.apply(Math, arNum);
+ 
+//     // In giá trị ra màn hình
+//     console.log("Giá trị lớn nhất là " +maxInNumbers);
+//     console.log("Giá trị nhỏ nhất là " +minInNumbers);
+// }
+
+// // Bài 7
+// {
+//     const arr = [3, 4, 6, -9, 10, -88, 2];
+//     let inNum = Number(prompt('Tìm 1 số trong mảng'));
+//     let index = arr.indexOf(inNum);
+//     if(index != -1)
+//     {
+//         console.log(`Số ${inNum} có trong mảng`);
+//     }
+//     else
+//     {
+//         console.log(`Số ${inNum} không có trong mảng`);
+//     }
+// }
+// let sheepsizeArr = [5,7,300,90,24,50,75];
+// function cau81()
+// {
+//     console.log('Hello, my name is Hieu, and here is my Sheep sizes');
+//     console.log(sheepsizeArr.join(" "));
+    
+// }
+// function cau82()
+// {
+//     var maxsizeSheep = Math.max.apply(Math,sheepsizeArr);
+//     console.log(`Now my biggest sheep has size ${maxsizeSheep}, let's save it`);
+// }
+
+// function cau83()
+// {
+//     let index = sheepsizeArr.indexOf(Math.max.apply(Math,sheepsizeArr));
+//     sheepsizeArr[index] = 8;
+//     console.log('After shearing, he is my flock');
+//     console.log(sheepsizeArr.join(" "));
+// }
+
+// function cau84(i)
+// {
+//     console.log(`MONTH ${i}`);
+//     console.log('One month has, passed, my sheeps has grown, here are their size');
+//     for(let i = 0;i<sheepsizeArr.length;i++)
+//     {
+//         sheepsizeArr[i] += 50;
+//     }
+//     console.log(sheepsizeArr.join(" "));
+// }
+
+// function cau85()
+// {
+//     for(i=1;i<=4;i++)
+//     {
+//         cau82();
+//         cau83();
+//         cau84(i);
+//     }
+// }
+
+// function cau86()
+// {
+//     //let sizeSheep = sheepsizeArr.split(",");
+//     let sumSize = 0;
+    
+//     for(i=0;i<sheepsizeArr.length;i++)
+//     {
+//         sheepsizeArr[i] = Number(sheepsizeArr[i]);
+//         sumSize +=sheepsizeArr[i];   
+//     }
+//     console.log(`My flock has size in total: ${sumSize}`);
+//     console.log(`I would get ${sumSize} * 2$ = ${sumSize*2} `)
+
+// }
+// // cau81()
+// // cau85()
+// // cau86()
+
+
+// // Bài 10
+// {
+//     let name = prompt("Enter a sequence of names");
+//     console.log(name);
+//     let aName = name.split(",");
+//     for(i=0;i<aName.length;i++)
+//     {
+//         aName[i]= '<'+aName[i]+'>';
+//     }
+//     alert(name+'=>'+aName);
+// }
+
+// // Bài 11
+// {
+//     let listNum = prompt("Nhập 1 dãy số, mỗi số ngăn cách nhau bằng dấu phẩy(,), in ra màn hình dãy số lẻ");
+//     let aNum = listNum.split(",");
+//     let oddNum = [];
+//     for(i=0;i<aNum.length;i++)
+//     {
+        
+//         if(aNum[i]%2 != 0)
+//         {
+//             oddNum.push(aNum[i]);
+//         }
+//     }
+//     alert(listNum+' => '+oddNum);
+// }
+
+
+
+
+
